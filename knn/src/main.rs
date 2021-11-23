e0beca598f7e53c6f39b74a12d71f611de81ce56
Javier <javierenriqueos@gmail.com>
2021-11-23 15:41:45 -0500

    updt

--- knn/src/main.rs
+++ knn/src/main.rs
@@ -1,15 +1,14 @@
 extern crate csv;
 mod classes;
-use crate::classes::persona::Persona;
 use crate::classes::data_frame::DataFrame;
+use crate::classes::persona::Persona;
 use std::time::Instant;
-
 
 /**
  * Grupo
- * Javier Olazábal 
- * Sebastian Chávarry 
- * Fiorella Valdivia 
+ * Javier Olazábal
+ * Sebastian Chávarry
+ * Fiorella Valdivia
  * Fabrizio figari
 */
 
@@ -17,14 +16,14 @@ fn main() {
     //se lee la data desde el csv
     let mut data = DataFrame::read_csv("./data.csv", true);
     //creamos la persona y pedimos por consola los datos
-    let mut persona = Persona{
-        gender : String::from("Female"),
-        age : 15.0,
-        altura : 1.7,
-        peso : 80.0, 
-        family_overweight : String::from("yes"),
+    let mut persona = Persona {
+        gender: String::from("Female"),
+        age: 15.0,
+        altura: 1.7,
+        peso: 80.0,
+        family_overweight: String::from("yes"),
         favc: String::from("yes"),
-        fcvc: 2.0, 
+        fcvc: 2.0,
         ncp: 2.0,
         caec: String::from("Always"),
         smoke: String::from("no"),
@@ -34,16 +33,14 @@ fn main() {
         tue: 0.0,
         calc: String::from("no"),
         mtran: String::from("Walking"),
-        nobey : String::from("")
+        nobey: String::from(""),
     };
-
-    // asignamos un k 
-    let k = 30;    
+    let k = 30;
     //hacemos el knn que nos dará como resultado el tipo de obesidad
     let start = Instant::now();
     persona.nobey = data.knn(k, &persona);
     let elapsed = start.elapsed();
     // ejecutar con cargo run --release
-    println!("Tiempo de ejecucion: {:?}" , elapsed);
-    println!("La clasificación de la persona es: {:?}", persona.nobey); 
+    println!("Tiempo de ejecucion: {:?}", elapsed);
+    println!("La clasificación de la persona es: {:?}", persona.nobey);
 }
